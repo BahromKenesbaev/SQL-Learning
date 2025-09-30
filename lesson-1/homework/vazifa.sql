5d8c40f59fbc5044836a7f410d3d9a44941f905e
BahromKenesbaev <baxromibnbaxadur@gmail.com>
2025-09-30 19:34:29 +0500

    Create vazifa.sql

---
+++ lesson-1/homework/vazifa.sql
@@ -0,0 +1,15 @@
+/*
+sql = structured query language
+database = malumotlar bazasi
+relational database = tablelar ulangan bolsa relational database hisoblanadi
+*/
+
+--SQL COMMANDS
+--Bu komandalar sqlni eng kerakli komandalari
+DQL - Data query language = select 
+DDL = Data definition language = Create, alter, drop, truncate
+DML = Data Manipulation Language = insert, update, delete
+DCL = Data Control Language = Grant, Revoke
+TCL = Transaction Control Language = begin tran, Commit, Rollback
+
+
